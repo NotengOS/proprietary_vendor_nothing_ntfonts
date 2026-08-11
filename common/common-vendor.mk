ddfea2e19b55ba184bdbe3c802ba0ae590840e8b
fiqys <elsifycritic@gmail.com>
2026-08-11 22:56:02 +0300

    ntfonts: Generate from FroggerPro_B4.1-260323-1635
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ common/common-vendor.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/nothing/ntfonts/common
+
+PRODUCT_COPY_FILES += \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-Italic.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-Italic.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-Light.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-Light.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-LightItalic.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-LightItalic.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-Medium.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-Medium.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-MediumItalic.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-MediumItalic.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/LetteraMonoLL-Regular.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/LetteraMonoLL-Regular.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/NDot55Caps.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NDot55Caps.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/NDot57Caps.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NDot57Caps.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/NType82-Headline.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NType82-Headline.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/NType82-Regular.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/NType82-Regular.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/Ndot-55.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ndot-55.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/Ndot-57-Aligned.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ndot-57-Aligned.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/Ndot-57.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ndot-57.otf \
+    vendor/nothing/ntfonts/common/proprietary/product/fonts/Ndot77JPExtended.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Ndot77JPExtended.ttf
